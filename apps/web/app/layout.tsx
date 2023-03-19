57275880ed3ae7da6bf20cf568606ed512c87aa3
mdshofiur <shofiurbd13@gmail.com>
2023-03-19 21:28:01 +0600

    remote cache

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-head-element */
+import { Analytics } from "@vercel/analytics/react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
