cda527a0b11d1e515265bea92b09cf89057b1d46
mdshofiur <shofiurbd13@gmail.com>
2023-03-19 21:54:32 +0600

    cache added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web Hello Remote Cache</h1>
+      <h1>Web Hello Remote Cache!!!</h1>
       <Button />
     </div>
   );
