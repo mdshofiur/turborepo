915e078cadea6680d0f0495a95682e3aab9b98e8
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-26 22:03:42 +0600

    get data from input using useRef

--- apps/react-redux/app/store/page.tsx
+++ apps/react-redux/app/store/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { StoreContext } from '@/context/store-context';
-import React from 'react';
+import React, { useRef } from 'react';
 import {  useContext } from 'react';
 
 
@@ -9,12 +9,32 @@ const Page = () => {
 
     const store = useContext(StoreContext)
     
-    console.log("🚀 store:", store)
+    const ref = useRef<HTMLInputElement>(null)
+
+    const [inputList, setInputList] = React.useState<string[]>([])
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => { 
+        e.preventDefault();
+        if(!ref.current?.value) return;
+        setInputList([...inputList, ref.current!.value])
+        ref.current!.value = ''
+    };
+
+    console.log(inputList)
 
     return (
-        <div>
-            Theme: {store}
-        </div>
+        <section className='container mx-auto py-12 bg-slate-400 min-h-screen px-5'>
+            <p>Theme: {store}</p>
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" ref={ref} className='p-3 rounded-md focus:outline-none my-3' />  <br />
+                <button type="submit" className='bg-blue-500 p-2 rounded-md'>Submit</button>
+                <div className='flex flex-wrap items-center gap-2 mt-3'>Input List: {
+                    inputList?.map((item, index) => {
+                        return <p key={index} className='bg-blue-500 p-2 rounded-md mx-2 w-fit '>{item}</p>
+                    })
+                }</div>
+            </form>
+        </section>
     );
 };
 
